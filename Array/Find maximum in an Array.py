ad5ff379b739581decd4eb1d38c4c0a6806329a8
Ajesh patel <ajeshpatel10@gmail.com>
2024-10-13 23:17:34 +0530

    Find maximum in an Array

---
+++ Array/Find maximum in an Array.py
@@ -0,0 +1,26 @@
+"""
+Find maximum in an Array
+
+Given a list of NN integers, representing height of mountains. Find the height of the tallest mountain.
+
+Input:
+    First line will contain T, number of testcases. Then the testcases follow.
+    The first line in each testcase contains one integer, N.
+    The following line contains N space separated integers: the height of each mountains.
+
+Output:
+For each testcase, output one line with one integer: the height of the tallest mountain for that test case.
+
+Constraints:
+    1≤T≤10
+    1≤N≤100000
+    0≤ height of each mountain ≤109
+
+Sample 1:
+Input:
+1
+5
+4 7 6 3 1
+Output:
+7
+"""

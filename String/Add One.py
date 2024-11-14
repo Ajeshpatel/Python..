f79bcdcad504d8e45b33e518258c7f2a999bb3cb
Ajesh patel <ajeshpatel10@gmail.com>
2024-11-14 22:27:47 +0530

    Add One

--- String/Add One.py
+++ String/Add One.py
@@ -1,26 +1,28 @@
 """
 Add One
-
 You are given a large number N. You need to print the number N+1.
 
 Note: The number is very large and it will not fit in standard integer data type. You have to take 
 the input as String and then manipulate the digits to convert it to N+1.
 
 Input Format:
+
     The first line of the input contains a single integer T - the number of test cases. 
     The description of T test cases follows.
 
     The first line of each test case contains a single integer N.
 
 Output Format:
+
     For each test case, print a single line containing one integer - the number N+1N+1.
 
 Constraints:
+
     1≤T≤100
     1≤N≤10200000−1
     the sum of the number of digits of all N in a single test file does not exceed 4⋅10**5
 
-Subtasks
+Subtasks:
 
 Subtask #1 (30 points):
 
